a63a8204408b7c8a1f4c20a8cab0fcc4422e9715
Nathakit Leemeetham <77628492+JackzazaTH@users.noreply.github.com>
2025-11-01 22:30:03 +0700

    Delete ddhouse/app/actions.ts

--- ddhouse/app/actions.ts
+++
@@ -1,9 +0,0 @@
-// app/actions.ts
-"use server";
-import { neon } from "ep-lingering-fire-a1mif4bh-pooler.ap-southeast-1.aws.neon.tech";
-
-export async function getData() {
-    const sql = neon(process.env.DATABASE_URL);
-    const data = await sql`neondb`;
-    return data;
-}
