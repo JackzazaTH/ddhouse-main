153b8b95a28f000d3fbab4765f36179fb29e180c
Nathakit Leemeetham <77628492+JackzazaTH@users.noreply.github.com>
2025-11-01 17:10:04 +0700

    Update data.ts

--- ddhouse/lib/data.ts
+++ ddhouse/lib/data.ts
@@ -1,4 +1,4 @@
-import { sql } from '@vercel/postgres';
+import { sql } from 'postgres://53a2035e8dbd7185af2e9ee6ffb28fc3eeaab91556e725d8183d344f96a1c000:sk_mv9cqhEoO6_TQco_XjRCF@db.prisma.io:5432/postgres?sslmode=require';
 import { HomeDesign } from './types';
 import { unstable_noStore as noStore } from 'next/cache';
 
