6fa0e2ea57b5224ee047bf57e8d22f9c8616cc8d
Nathakit Leemeetham <77628492+JackzazaTH@users.noreply.github.com>
2025-11-01 22:25:18 +0700

    Update actions.ts

--- ddhouse/app/actions.ts
+++ ddhouse/app/actions.ts
@@ -4,6 +4,6 @@ import { neon } from "@neondatabase/serverless";
 
 export async function getData() {
     const sql = neon(process.env.DATABASE_URL);
-    const data = await sql`...`;
+    const data = await sql`neondb`;
     return data;
 }
