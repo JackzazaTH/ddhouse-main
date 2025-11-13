a334e3894f8fae5abfeee5e28a204fb4cd33d1f2
Nathakit Leemeetham <77628492+JackzazaTH@users.noreply.github.com>
2025-11-13 12:19:38 +0700

    Delete ddhouse directory

--- ddhouse/constants.tsx
+++
@@ -1,2 +0,0 @@
-// This file is part of a legacy Vite setup and is not used in the Next.js application.
-// Its content has been cleared to avoid confusion. The active constants are in /lib/constants.ts.
